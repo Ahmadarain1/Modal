f4309e7c4ad9c86c64a77133173836119245ce8e
Ahmad <ahmadmian803@gmail.com>
2023-04-18 12:13:31 -0700

    Initial commit

---
+++ modal.js/modal.js
@@ -0,0 +1,21 @@
+if(data1.style.display = 'block'){
+    data1.style.display = 'none';
+}
+function openbtn(){
+    let btn1 = document.getElementById('btn1');
+    let data1 = document.getElementById('data1');
+    if(data1.style.display = 'none'){
+        data1.style.display = 'block';
+    }else{
+        console.log('error');
+    }
+}
+function closebtn(){
+    let btn2 = document.getElementById('btn2');
+    let data1 = document.getElementById('data1');
+    if(data1.style.display = 'block'){
+        data1.style.display = 'none';
+    }else{
+        console.log('error');
+    }
+}
